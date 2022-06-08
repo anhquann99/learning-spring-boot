1bf9895b661625a9cb1daf189b9a8e7d2f0895b2
QuanDA <quanda@gmail.com>
2022-06-08 17:27:25 +0700

    Hello Maven

---
+++ hello-maven/src/main/java/Test.java
@@ -0,0 +1,8 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class Test {
+    public static void main(String[] args) {
+        boolean check = StringUtils.isNumeric("123");
+        System.out.println(check);
+    }
+}
